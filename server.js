a232066113014051b21bd5f018fdb82c93c90677
Sonny <richardhughesjr@gmail.com>
2018-03-03 17:55:53 -0600

    Add Express Server

---
+++ server.js
@@ -0,0 +1,19 @@
+//DEPENDENCIES
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+//CREATE THE EXPRESS SERVER
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
